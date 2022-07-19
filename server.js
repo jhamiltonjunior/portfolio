d8068d2336f349a776b707b7dd095446e73b2de0
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-07-19 20:27:26 -0300

    feature: more images added in work

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(express.static(`${__dirname}`))
 
+app.get('/blog', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
