9ba01aa8877cf9150f9df1d169d5a81cd7976ac5
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-15 20:55:15 -0300

    url changed to try index on google

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 })
 
 let sitemap
-app.get('/sitemap', function(req, res) {
+app.get('/sitemap.xml', function(req, res) {
   res.header('Content-Type', 'application/xml');
   res.header('Content-Encoding', 'gzip');
   // if we have a cached entry send it
@@ -31,7 +31,7 @@ app.get('/sitemap', function(req, res) {
     const pipeline = smStream.pipe(createGzip())
 
     // pipe your entries or directly write them.
-    smStream.write({ url: '/',  changefreq: 'daily', priority: 0.3 })
+    smStream.write({ url: '/.',  changefreq: 'daily', priority: 0.3 })
     /* or use
     Readable.from([{url: '/page-1'}...]).pipe(smStream)
     if you are looking to avoid writing your own loop.
