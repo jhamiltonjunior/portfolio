b4f28f3decc1808620e461bc945a316b4eb1b95d
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-15 20:37:31 -0300

    sitemap dependencie added, robots.txt file added and route get added to get sitemap

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const path = require('node:path')
+const { SitemapStream, streamToPromise } = require('sitemap')
+const { createGzip } = require('zlib')
+const { Readable } = require('stream')
 
 const port = 8080;
 
@@ -13,18 +16,37 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.get("/sitemap", function (req, res) {
-  var fs = require("fs");
+let sitemap
+app.get('/sitemap', function(req, res) {
+  res.header('Content-Type', 'application/xml');
+  res.header('Content-Encoding', 'gzip');
+  // if we have a cached entry send it
+  if (sitemap) {
+    res.send(sitemap)
+    return
+  }
 
   try {
-    var data = fs.readFileSync("5289617_2.xml", "utf8");
-    res.send(data);
-    // console.log(data.toString());
-  } catch (e) {
-    console.log("Error:", e.stack);
-  }
-});
+    const smStream = new SitemapStream({ hostname: `https://${req.headers.host}` })
+    const pipeline = smStream.pipe(createGzip())
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+    // pipe your entries or directly write them.
+    smStream.write({ url: '/',  changefreq: 'daily', priority: 0.3 })
+    /* or use
+    Readable.from([{url: '/page-1'}...]).pipe(smStream)
+    if you are looking to avoid writing your own loop.
+    */
+
+    // cache the response
+    streamToPromise(pipeline).then(sm => sitemap = sm)
+    // make sure to attach a write stream such as streamToPromise before ending
+    smStream.end()
+    // stream write the response
+    pipeline.pipe(res).on('error', (e) => {throw e})
+  } catch (e) {
+    console.error(e)
+    res.status(500).end()
+  }
+})
+
+app.listen(port);
